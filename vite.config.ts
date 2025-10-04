713b21043524a4d08456d46e226954f3365bdea6
Dev Dixit <devdixitsocial@gmail.com>
2025-10-04 20:05:14 +0530

    trying to find bugs, not resolved yet

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: true,
+    port: 8080
   },
   plugins: [
     react(),
